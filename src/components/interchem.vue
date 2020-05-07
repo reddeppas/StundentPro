45366a1748e9904da57ccac72a32e4c63b102bf8
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-07 19:33:00 +0530

    Added title pages for intermpc

--- src/components/interchem.vue
+++ src/components/interchem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid mt-4">
-    <h1 class="h1">Posts Manager</h1>
+    <h1 class="h1">Inter Chemistry</h1>
     <p>Only authenticated users should see this page</p>
   </div>
 </template>
