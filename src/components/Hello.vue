e2f2a9119947aff96034b8ae651fb38c0c3708af
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-07 20:46:27 +0530

    Home page update

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,7 @@
   <div class="hero">
     <div>
       <h1 class="display-3">Hello World</h1>
-      <p class="lead">This is the homepage of your vue app</p>
+      <p class="lead">Welcome to Intermediate Student Training Portal. StudentPro will provide practical based trainings </p>
     </div>
   </div>
 </template>
