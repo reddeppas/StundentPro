08723fc3b78730f06261e8d42f885987330fcc0a
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-08 16:50:11 +0530

    fixing indentation

--- src/components/intermaths.vue
+++ src/components/intermaths.vue
@@ -2,9 +2,9 @@
   <div class="container-fluid mt-4">
     <h1 class="h1">Maths</h1>
     <p>Only authenticated users should see this page</p>
+    <vimeo-player ref="player" :video-id="videoID" @ready="onReady" :player-height="height">
+    </vimeo-player>
   </div>
-  <vimeo-player ref="player" :video-id="videoID" @ready="onReady" :player-height="height">
-  </vimeo-player>
 </template>
 <script>
 export default {
