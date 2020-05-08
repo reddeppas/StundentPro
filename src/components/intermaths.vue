15e86680ce148a365ae817c9b4835fedabea321b
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-08 16:34:24 +0530

    Adding dnsmasq attrs

--- src/components/intermaths.vue
+++ src/components/intermaths.vue
@@ -1,10 +1,3 @@
-import { vueVimeoPlayer } from 'vue-vimeo-player'
-
-export default {
-    data: {},
-    components: { vueVimeoPlayer }
-}
-
 <template>
     <vimeo-player ref="player" :video-id="videoID" @ready="onReady" :player-height="height">
     </vimeo-player>
