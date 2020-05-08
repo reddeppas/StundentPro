c28c9bacd492c00f6c17906fd5a771c300e65aa2
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-08 16:45:08 +0530

    fixing indentation

--- src/components/intermaths.vue
+++ src/components/intermaths.vue
@@ -9,23 +9,23 @@
 <script>
 export default {
 data() {
-    return {
-        videoID: '141851770',
-        height: 600,
-        options: {},
-        playerReady: false,
-    }
+  return {
+    videoID: '141851770',
+    height: 600,
+    options: {},
+    playerReady: false,
+  }
 },
 methods: {
-    onReady() {
-        this.playerReady = true
-    },
-    play () {
-        this.$refs.player.play()
-    },
-    stop () {
-        this.$refs.player.stop()
-    }
+  onReady() {
+    this.playerReady = true
+  },
+  play () {
+    this.$refs.player.play()
+  },
+  stop () {
+    this.$refs.player.stop()
+  }
 }
 }
 </script>
