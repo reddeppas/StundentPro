5086e035bc9c9d1ec837819464c8d6a61365698a
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-09 09:08:54 +0530

    fixing indentation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/home'
+import home from '@/components/home'
 import cks from '@/components/cks'
 import devops from '@/components/devops'
 import cicd from '@/components/cicd'
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import home from '@/components/home'
 
-describe('Hello.vue', () => {
+describe('home.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(home)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js PWA')
