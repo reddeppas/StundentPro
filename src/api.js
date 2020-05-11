a93101ba9ed33f39dac2c2a953b4600acd2812b8
Reddeppa S <rsandhireddygari@gomoxie.com>
2020-05-11 18:18:13 +0530

    Moving to https

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'https://xpertdevops.com',
+  baseURL: 'http://xpertdevops.com:8081',
   json: true
 })
 
